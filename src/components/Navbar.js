1868c380de94ed9d5c5aa2c4b7426ba886b6b0ae
kaori <suige787@live.jp>
2018-09-26 22:02:13 -0700

    :lipstick: Change navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
+import codepen from '../img/codepen-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
@@ -9,24 +10,26 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="suige" style={{ width: '30px' }} />
           </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://codepen.io/suige/"
           target="_blank"
           rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={codepen} alt="Codepen" />
+          </span>
+        </a>
+        <a
+            className="navbar-item"
+            href="https://github.com/suige/"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           <span className="icon">
             <img src={github} alt="Github" />
